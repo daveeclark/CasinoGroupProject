62f33f518125d89eb8db604719b0596475267098
daveeclark <dclark2016@hotmail.com>
2019-10-16 11:13:57 -0400

    first commit

---
+++ Desktop/CasinoGroupProject-master/src/main/java/io/zipcoder/casino/players/Player.java
@@ -0,0 +1,13 @@
+package io.zipcoder.casino.players;
+
+public class Player {
+  private String name;
+
+  public Player(String name){
+    this.name = name;
+  }
+
+  public void gamesPlayed(Game game){
+
+  }
+}
